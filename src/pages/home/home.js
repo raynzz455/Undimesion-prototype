93538889f92259d10bacb7763461193bba6e8248
Raynaldi <raynss455x@gmail.com>
2024-10-28 12:57:01 +0700

    Ui: Update & added card

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './home.css';
+import { TypeAnimation } from 'react-type-animation';
 
 const Countries = [
   {
@@ -61,7 +62,7 @@ function Home() {
 
           <div className='home-hopes-container'>
             
-            <div className='home-hopes-section1'>
+            <div className='home-hopes-section'>
               <div className='home-images-container'>
                 <div className='indonesia-pictures' />
               </div>
@@ -75,7 +76,7 @@ function Home() {
                 </div>
               </div>
             </div>
-            <div className='home-hopes-section1'>
+            <div className='home-hopes-section'>
             <div className='home-images-container'>
             <div className='bogor-pictures'/>
               </div>
@@ -89,22 +90,71 @@ function Home() {
                 </div>
               </div>
             </div>
-            <div className='home-hopes-section1'>
+            <div className='home-hopes-section'>
             <div className='home-images-container'>
             <div className='future-pictures'/>
               </div>
-              <p className='home-indonesia'>Hopes & Dreams <br/> to Our Future</p>
+              <p className='home-indonesia'>Everything to Our Future</p>
               <div className='home-indonesia-text-container'>
                 <div className='home-indonesia-text-quote'>
                 <p>{Countries[currentQuote].text}</p>
                 </div>
-                <div className='home-indonesia-text-owner'>
+                <div className='home-indonesia-text-owner'> 
                   <p>-{Countries[currentQuote].author}</p>
                 </div>
               </div>
             </div>
+            
+            <div className='home-container-2'>
+
+              <div className='home-another'>
+                <div className='home-another-card'>
+                  <div className='w-full h-full p-5 flex justify-center relative'>
+                  <p className='absolute left-3 top-2'>"</p>
+                  <div className='p-3 flex justify-center items-center home-card-text'>
+                  <TypeAnimation
+                      sequence={[
+                        'In our abilities, we find our unwavering faith.',
+                        5000, 
+                        'In our convictions, we discover steadfast belief.',
+                        5000,
+                        'In our future, we place unconditional trust.',
+                        5000,
+                        'In our divine, we place our sacred faith.',
+                        5000,
+                        'In ourselves, we find our true confidence.',
+                        5000
+                      ]}
+                      wrapper="span"
+                      speed={30}
+                      style={{ fontSize: '1em', display: 'inline-block', fontFamily:'Hedvig Letters Serif'}}
+                      repeat={Infinity}
+                    />
+                  </div>
+                  <p className='absolute right-3 bottom-2'>"</p>
+                  </div>
+
+                </div>
+              </div>
+
+                      <div className='home-teams w-screen'>
+                        <p className='text-white'>
+                          Our Team
+                          </p>
+                          
+
+                      </div>
+
+
+
+            </div>
           
           </div>
+          <div>
+            
+          </div>
+
+
         </div>
       </div>
     </div>
